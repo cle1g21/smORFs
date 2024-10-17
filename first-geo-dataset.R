efff51191c020c54ec0e8252c8e4ee3fe5149ed6
C.L.Ellison <cle1g21@cyan51.cluster.local>
2024-10-17 11:58:41 +0100

    Added header to design table

--- first-geo-dataset.R
+++ first-geo-dataset.R
@@ -1,9 +1,8 @@
 library(GEOquery)
-??GEOquery
 
 count_table <- read.csv("GSE273464_raw_counts_GRCh38.p13_NCBI.tsv", header = TRUE, sep = "\t")
 head(count_table)
 as.data.frame(count_table)
 dim(count_table)
 
-design_table <- read.csv("design_tags.csv", header = FALSE)
+design_table <- read.csv("design_tags.csv", header = TRUE)
