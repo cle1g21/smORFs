806a65311d67b33e79dcc0baaccb0371ad09b43a
C.L.Ellison <cle1g21@cyan51.cluster.local>
2024-10-17 13:03:48 +0100

    continuation of DESeq2, trying to stop error when creating matrix

--- first-geo-dataset.R
+++ first-geo-dataset.R
@@ -1,4 +1,5 @@
 library(GEOquery)
+library(DESeq2)
 
 count_table <- read.csv("GSE273464_raw_counts_GRCh38.p13_NCBI.tsv", header = TRUE, sep = "\t")
 head(count_table)
@@ -6,3 +7,10 @@ as.data.frame(count_table)
 dim(count_table)
 
 design_table <- read.csv("design_tags.csv", header = TRUE)
+colnames(design_table)
+
+dds <- DESeqDataSetFromMatrix(countData = count_table,
+                              colData = design_table,
+                              design = ~rhPOSTN)
+
+colnames(design_table) <- c("GeneID", "condition")
