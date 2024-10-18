46f4eefb9d913cafb2d5f424a845616daed22891
C.L.Ellison <cle1g21@cyan52.cluster.local>
2024-10-18 16:56:13 +0100

    perform DESeq2 on 2 more datasets. all no significant padj... must be doing something wrong, cehck with Owen

--- first-geo-dataset.R
+++ first-geo-dataset.R
@@ -1,45 +1,101 @@
 library(GEOquery)
 library(DESeq2)
-
 library(dplyr)
 
 #read Owens raw counts and sample-info
 rna <- read.delim("rna_counts.txt", sep = "\t", header = TRUE)
 
+#read sample-info
 coldata <- read.delim("sample_info.txt", sep = "\t")
 coldata <-as.data.frame(apply(coldata, 2, as.factor))
 head(coldata)
 
+#check that sample names match
 all(colnames(rna) == rownames(coldata))
 colnames(rna)
 rownames(coldata)
 
+#make DESeqDataSet
 dds <- DESeqDataSetFromMatrix(countData = rna,
                               colData = coldata,
                               design = ~Condition)
+dds
+
+#run DESeq
+dds <- DESeq(dds)
+res <- results(dds)
+print(res)
 
 
-#changed column names to match design_GSE273142
-colnames(rna) <- c("GeneID", "GSM8422516", "GSM8422517", "GSM8422518", "GSM8422519", "GSM8422520", "GSM8422521")
+################################################################# try using DeSeq2 with GEO dataset GSE61405
+#read GEO dataset GSE61405 raw counts and sample-info
+rna <- read.delim("GSE61405_raw_counts.tsv", sep ="\t", header = TRUE)
+rna
+head(rna)
 
-colnames(coldata)|>
-    select(c("Pao24RIBO24hpA5R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam", "Pao27RIBO24hpA10R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam	2", "Pao34RIBO24hpA15R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam", "Pao41RIBO24hpA20R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam", "Pao24RIBOBSLpA1R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam", "Pao27RIBOBSLpA6R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam", "Pao34RIBOBSLpA11R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam", "Pao41RIBOBSLpA16R1001trimmedL29NoAbundant.fastq.gzAligned.sortedByCoord.out.bam"))
+coldata <- read.table("sample_info.csv", sep = ",", header = TRUE)
+coldata <-as.data.frame(apply(coldata, 2, as.factor))
+head(coldata)
+rownames(coldata)
 
-#create data.frame
-as.data.frame(tab)
+#check that sample names match
+colnames(coldata)[1] <- "GeneID"
+colnames(coldata)
+head(coldata)
+colnames(rna)
 
-#read design .csv 
-design <- read.csv("design_GSE273142.csv", header = TRUE)
+#remove GeneID column
+gene_ids <- rna$GeneID
+rna_clean <- rna[, -1]
+colnames(rna_clean)
 
-#removes GeneID column ready for DeSeq2
-gene_ids <- tab$GeneID
-count_table_clean <- tab[, -1]
+#make DESeqDataSet
+dds <- DESeqDataSetFromMatrix(countData = rna_clean,
+                              colData = coldata,
+                              design = ~Condition)
+dds
 
+#make sure comparison is against control
+dds$Condition <-relevel(dds$Condition, ref = "control")
 
-#construct DeSeq2 dataset
-dds <- DESeqDataSetFromMatrix(countData = count_table_clean,
-                              colData = design_clean,
-                              design = ~condition)
+#run DESeq
+dds <- DESeq(dds)
+res <-results(dds, alpha = 0.05)
+res
 
-rownames(design_clean)
-rownames(count_table_clean)
+#check for significant genes
+significant_genes <- rownames(res)[which(res$padj < 0.05)]
+head(significant_genes)
+length(significant_genes)
+summary(res)
+
+################################################################# try using DeSeq2 with GEO dataset GSE79666
+#read GEO dataset GSE79666 raw counts and sample-info
+rna <- read.delim("GSE79666_raw_counts_GRCh38.p13_NCBI.tsv", sep ="\t", header = TRUE)
+
+coldata <- read.table("GSE79666_info.csv", sep = ",", header = TRUE)
+coldata <-as.data.frame(apply(coldata, 2, as.factor))
+colnames(coldata)[1] <- "GeneID"
+
+#remove GeneID column
+gene_ids <- rna$GeneID
+rna_clean <- rna[, -1]
+colnames(rna_clean)
+
+#make DESeqDataSet
+dds <- DESeqDataSetFromMatrix(countData = rna_clean,
+                              colData = coldata,
+                              design = ~Condition)
+dds
+
+#make sure comparison is against control
+dds$Condition <-relevel(dds$Condition, ref = "CTRL")
+dds <- DESeq(dds)
+res <-results(dds, alpha = 0.05)
+res
+
+#check for significant genes
+significant_genes <- rownames(res)[which(res$padj < 0.05)]
+head(significant_genes)
+length(significant_genes)
+summary(res)
