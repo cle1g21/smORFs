bdd50fd5f98b6006d5a46f3fe423c3490a13a499
C.L.Ellison <cle1g21@cyan53.cluster.local>
2024-10-30 13:20:53 +0000

    saved updated first geo dataset and new file i am working on

--- DESeq2_GSE2683666.r
+++ DESeq2_GSE2683666.r
@@ -4,3 +4,10 @@
 library(GEOquery)
 library(DESeq2)
 library(dplyr)
+
+#read GEO dataset GSE2683666 counts
+cts <- as.matrix(read.csv("GSE268366_raw_counts_GRCh38.p13_NCBI.tsv", sep = "\t", header = TRUE))
+head(cts)
+
+#read coldata
+coldata <- read.csv("GSE268366_coldata.csv", sep = "\t")
