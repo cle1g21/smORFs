e7d41c75f38682d452d7bf03e7286f90fd971308
C.L.Ellison <cle1g21@cyan52.cluster.local>
2025-01-27 18:25:06 +0000

    Tried DESeq2 on ESCA TCGA data. No nuORFs matched DEGs

--- TCGA/test.r
+++ TCGA/test.r
@@ -1,5 +1,5 @@
 #** First back after Christmas. Checked access and viewing TCGA data **
-data <- fread("/mainfs/ddnb/TCGA_data/full_TCGA-DLBC_matrix.csv")
+data <- read.csv("/mainfs/ddnb/TCGA_data/full_TCGA-DLBC_matrix.csv")
 View(data)
 
 dim(data) # 60660 genes, 49 samples
